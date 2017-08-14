50e080d49eb92ab38c01b41c26b567be8d00af92
=Isaac <=trevex2000@live.com>
2017-08-14 21:42:19 +0300

    first commit for making coffe

---
+++ coffee.py
@@ -0,0 +1,13 @@
+'''
+Andela Making Coffee App 
+'''
+#Make my coffee
+
+ingredients = ['coffee', 'hot water']
+print ('Started making coffee...')
+print('Getting cup')
+print('Adding {}'.format(' and '.join(ingredients)))
+print('Stir the mix')
+print('Finished making coffeee...')
+my_coffee = 'Tasty Coffee'
+print("--Here's your {}, Enjoy!!-- Mr. {} \n".format(my_coffee,'Evans'))
